961a9092f7a7b0a5eaa6c4b62f8a2966bf6366e8
Lampart_Dang_Thai_Ngoc_Thach <ngoc_thach@lampart-vn.com>
2025-11-19 14:27:42 +0700

    test

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,51 @@
+import AddTask from "@/components/AddTask";
+import DateTimeFilter from "@/components/DateTimeFilter";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import StatsAndFilters from "@/components/StatsAndFilters";
+import TaskList from "@/components/TaskList";
+import TaskListPagination from "@/components/TaskListPagination";
 import React from "react";
 
 const HomePage = () => {
-  return <h1 class="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="min-h-screen w-full bg-[#fefcff] relative">
+      {/* Dreamy Sky Pink Glow */}
+      <div
+        className="absolute inset-0 z-0"
+        style={{
+          backgroundImage: `
+        radial-gradient(circle at 30% 70%, rgba(173, 216, 230, 0.35), transparent 60%),
+        radial-gradient(circle at 70% 30%, rgba(255, 182, 193, 0.4), transparent 60%)`,
+        }}
+      />
+      {/* Your Content/Components */}
+      <div className="container pt-8 mx-auto relative z-10">
+        <div className="w-full max-w-2xl p-6 mx-auto space-y-6">
+          {/* Đầu Trang */}
+          <Header />
+
+          {/* Tạo nhiệm vụ */}
+          <AddTask />
+
+          {/* Thống kê và bộ lọc */}
+          <StatsAndFilters />
+
+          {/* Danh sách nhiệm vụ */}
+          <TaskList />
+
+          {/* Phân trang và lọc theo ngày */}
+          <div className="flex flex-col items-center justify-center gap-6 sm:flex-row">
+            <TaskListPagination />
+            <DateTimeFilter />
+          </div>
+
+          {/* Footer */}
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
