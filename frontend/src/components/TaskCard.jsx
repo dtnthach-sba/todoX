cd2958bc7bdc3bd957516d3e8c354a31765149d1
Lampart_Dang_Thai_Ngoc_Thach <ngoc_thach@lampart-vn.com>
2025-11-21 16:54:45 +0700

    commit

--- frontend/src/components/TaskCard.jsx
+++ frontend/src/components/TaskCard.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { Card } from "./ui/card";
+import { cn } from "@/lib/utils";
+import { CheckCircle2 } from "lucide-react";
 
 const TaskCard = ({ task, index }) => {
-  return <div>TaskCard</div>;
+  let isEditting = false;
+
+  return (
+    <Card
+      className={cn(
+        "p-4 bg-gradient-card border-0 shadow-custom-md hover:shadow-custom-lg transition-all duration-200 animate-fade-in group",
+        task.status === "complete" && "opacity-75"
+      )}
+      style={{ animationDelay: `${index * 50}ms` }}
+    >
+      <div className="flex items-center gap-4">
+        {/* nút tròn */}
+        <Button
+          variant="ghost"
+          size="icon"
+          className={cn(
+            "flex-shrink-0 size-8 rounded-full transition-all duration-200",
+            task.status === "complete"
+              ? "text-success hover:text-success/80"
+              : "text-muted-foreground hover:text-primary"
+          )}
+              >
+                  {task.status === 'complete' ? (
+                  <CheckCircle2></CheckCircle2>
+                  ):()}
+        </Button>
+      </div>
+    </Card>
+  );
 };
 
 export default TaskCard;
